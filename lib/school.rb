57b61794150cc19949d81fe105607f4e07aafd24
tirempbs <tirempbs@gmail.com>
2018-10-10 23:39:53 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require "pry"
+
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    students = []
+    @roster.select do |year, name|
+      if year == grade
+        students << name
+      end
+    end
+    students.flatten
+  end
+
+  def sort
+    @roster[9].sort!
+    @roster.sort.to_h
+  end
+
+end
